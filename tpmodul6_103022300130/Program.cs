8b24a256064d4a1118ad845265512547e58228ca
Rakha Fatih <rakhafatihathallah@gmail.com>
2025-03-23 23:42:57 +0700

    Menambahkan class SayaTubeVideo

--- tpmodul6_103022300130/Program.cs
+++ tpmodul6_103022300130/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
 
-namespace tpmodul6_103022300130
+
+class SayaTubeVideo
 {
-    class MainClass
+    private int id;
+    private string title;
+    private int playCount;
+    
+    public SayaTubeVideo(string title)
     {
-        public static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+        this.id = new Random().Next(10000, 100000);
+        this.title = title;
+        this.playCount = 0;
+    }
+
+    public void increasePlayCount(int num)
+    {
+        playCount += num;
+    }
+
+    public void printVideoDetails()
+    {
+        Console.WriteLine($"ID: [{id}] Title: [{title}] Play Count: [{playCount}]");
     }
 }
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design By Contract - RakhaFatihAthallah_103022300130");
+        sayaTubeVideo.increasePlayCount(10);
+        sayaTubeVideo.printVideoDetails();
+    }
+}
+
